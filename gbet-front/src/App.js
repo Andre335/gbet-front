88a4bed8c9c7fa5b74871801e383bdfc48d4f878
Andre Araujo Falcao Goncalves 113210893 <andreafg@lcc3-087.lcc.ufcg.edu.br>
2018-10-31 17:33:23 -0300

    Progress bar com cores e ajustada

--- gbet-front/src/App.js
+++ gbet-front/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       super();
       this.state = {
           lives: [{title: "Title 1", description: "Description 1", owner: "Owner 1", date: "23-10-2018", bets: [{in_favor: true}, {in_favor: false}]},
-                  {title: "Title 2", description: "Description 2", owner: "Owner 2", date: "24-10-2018", bets: [{in_favor: true}, {in_favor: false}]}]
+                  {title: "Title 2", description: "Description 2", owner: "Owner 2", date: "24-10-2018", bets: [{in_favor: true}]}]
       };
   }
 
--- gbet-front/src/components/BetProgressBar.js
+++ gbet-front/src/components/BetProgressBar.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import {Line} from 'rc-progress';
+import '../css/ProgressBar.css';
 
 class BetProgressBar extends Component {
+    constructor() {
+        super();
+        this.handleColor = this.handleColor.bind(this);
+    }
+
+    handleColor(progress) {
+        if (progress <= 33) {
+            return "#f44262";
+        } else if (progress > 33 && progress <= 66) {
+            return "#f4eb42";
+        } else {
+            return "#56f442";
+        }
+    }
+
     render() {
         return(
             <div className="progress-container">
-                <Line percent={(this.props.bets_in_favor/this.props.bets_total)*100} 
-                      strokeWidth="2" strokeColor="#000000"/>
+                <Line className="progress-bar" percent={(this.props.bets_in_favor/this.props.bets_total)*100} 
+                      strokeWidth="1" strokeColor={this.handleColor((this.props.bets_in_favor/this.props.bets_total)*100)}/>
             </div>
         )
     }
