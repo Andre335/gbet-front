42708b94e29327b3c213dbccf8c99b274b91038e
Andre Goncalves <andre335@gmail.com>
2018-10-31 23:36:44 -0300

    Sidebar basics

--- gbet-front/src/App.js
+++ gbet-front/src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import LiveList from './components/LiveList';
+import GbetNav from './components/GbetNav';
+import GbetSide from './components/GbetSide';
 
 class App extends Component {
   constructor() {
       super();
       this.state = {
           lives: [{title: "Title 1", description: "Description 1", owner: "Owner 1", date: "23-10-2018", bets: [{in_favor: true}, {in_favor: false}]},
-                  {title: "Title 2", description: "Description 2", owner: "Owner 2", date: "24-10-2018", bets: [{in_favor: true}]}]
+                  {title: "Title 2", description: "Description 2", owner: "Owner 2", date: "24-10-2018", bets: [{in_favor: true}]},
+                  {title: "Title 3", description: "Description 3", owner: "Owner 3", date: "25-10-2018", bets: [{in_favor: false}, {in_favor: false}, {in_favor: true}, {in_favor: false}]}]
       };
   }
 
   render() {
     return (
       <div className="App">
-        <LiveList className="live-list" lives={this.state.lives}/>
+        <GbetSide className="gbet-sidebar" lives={this.state.lives}/>
       </div>
     );
   }
